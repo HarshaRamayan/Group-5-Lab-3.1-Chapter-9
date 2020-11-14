1d377e398d0afacd8d06e9f28dc555e1b12e19b6
anishpanchadar <59576795+anishpanchadar@users.noreply.github.com>
2020-11-13 22:16:31 -0500

    Added EventListener function

--- Group 5 Lab3.1 -Chapter 9/ch09-project1-start-master/js/chapter09-project01.js
+++ Group 5 Lab3.1 -Chapter 9/ch09-project1-start-master/js/chapter09-project01.js
@@ -1,2 +1,18 @@
 
 /* add code here  */
+/* Coded by Anish Panchadara */
+document.addEventListener("DOMContentLoaded", theDomHasLoaded, false);
+
+function theDomHasLoaded(e) {
+  document.querySelectorAll('.hilightable').forEach(item => {
+    item.addEventListener('focus', event => {
+        if (item.classList.contains("error")) {
+            item.classList.remove("error");
+        }
+        item.classList.toggle("highlight");
+    });
+    item.addEventListener('blur', event => {
+      item.classList.toggle("highlight");
+    });
+  });
+}
